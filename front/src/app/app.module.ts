894a1866bd77bb4c0d3188c1972b7cfca68ff8b7
iruasev <jrua1984@gmail.com>
2018-05-20 21:41:03 +0200

    Front: - Routing - (Shared Components): Navbar, NotFoundComponent - (Components) Home, About - favicon - logo

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    AboutComponent
   ],
-  imports: [
-    BrowserModule
+  imports:      [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // {enableTracing: true} // <-- debugging purposes only
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
